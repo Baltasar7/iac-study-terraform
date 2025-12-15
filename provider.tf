83aa1d05a397ab80f56618c7e4e146316d2e9082
Baltaser7 <Baltaser7@example.com>
2025-12-16 03:37:31 +0900

    Configure backend s3

--- provider.tf
+++ provider.tf
@@ -6,6 +6,11 @@ terraform {
             version = "6.26.0"
         }
     }
+    backend "s3" {
+        bucket = "dev-tfstate-aws-iac-study"
+        key = "dev/terraform.tfstate"
+        region = "us-east-1"
+    }
 }
 
 provider "aws" {
