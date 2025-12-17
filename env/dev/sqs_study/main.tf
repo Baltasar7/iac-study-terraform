8fe86c003390d3d99a82b9cbdc24e72d38ff42fb
Baltaser7 <Baltaser7@example.com>
2025-12-18 06:04:07 +0900

    Update root module for add 2nd queue

--- env/dev/sqs_study/main.tf
+++ env/dev/sqs_study/main.tf
@@ -2,6 +2,12 @@ module "sqs_module_test" {
   source = "../../../usecases/sqs_study"
   stage = "dev"
   queue_name_suffix = "sqs-test"
+  sqs_queue_visibility_timeout_seconds = 60
+}
+module "sqs_module_test2" {
+  source = "../../../usecases/sqs_study"
+  stage = "dev"
+  queue_name_suffix = "sqs-test2"
 }
 
 output "sqs_queue_url" {
